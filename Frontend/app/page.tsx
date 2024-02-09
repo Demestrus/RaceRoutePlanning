a3369470c3951cd3017132d9e256a04a75231f4e
d.ichko <d.ichko@corp.mail.ru>
2024-02-09 23:57:33 +0300

    Oops fix 2

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -4,6 +4,7 @@ import { ChangeEvent, useEffect, useState } from 'react';
 import Chart from './chart/component';
 import getRaceRoute from './apiService';
 import ChartControls from './chart-controls/component';
+import { IRaceRoute } from './models';
 
 export default function Page() {
     const [data, setData] = useState<IRaceRoute>({
